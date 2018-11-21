e3654d997bb59490fad05211166105d2f9871d2b
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2018-11-21 21:12:40 +0200

    Welcome and StartStop Activities added. Linked them by pressing a button

---
+++ Pedometer/app/src/main/java/com/chs/pedometer/WelcomeActivity.java
@@ -0,0 +1,21 @@
+package com.chs.pedometer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(WelcomeActivity.this, StartStopActivity.class);
+        startActivity(intent);
+    }
+}
