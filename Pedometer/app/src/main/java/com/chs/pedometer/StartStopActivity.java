61b0491b848d52bac140760bbefc75099f0f4a93
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2018-11-22 20:32:12 +0200

    StartStop Activity improved. Added Maps Activity - without API Key

--- Pedometer/app/src/main/java/com/chs/pedometer/StartStopActivity.java
+++ Pedometer/app/src/main/java/com/chs/pedometer/StartStopActivity.java
@@ -1,7 +1,9 @@
 package com.chs.pedometer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartStopActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class StartStopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_stop);
+    }
+
+
+    public void onClickSeeOnMap(View v){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
