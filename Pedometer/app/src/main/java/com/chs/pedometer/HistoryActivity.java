368401b070df5fcab9bb3cb88ca8dc98b491d722
AFro13 <32522933+AFro13@users.noreply.github.com>
2019-01-06 20:58:48 +0200

    Buttons with dates

--- Pedometer/app/src/main/java/com/chs/pedometer/HistoryActivity.java
+++ Pedometer/app/src/main/java/com/chs/pedometer/HistoryActivity.java
@@ -15,41 +15,59 @@ import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 
 public class HistoryActivity extends AppCompatActivity {
 
+    private String fileName = "history10.json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        LinearLayout linear = (LinearLayout)findViewById(R.id.history_layout);
+        LinearLayout linear = (LinearLayout) findViewById(R.id.history_layout);
         Button btn1;
 
-        for (int i = 1; i <= 20; i++) {
+        History historyJSON = getHistoryJSON();
+        ArrayList<Route> routes = historyJSON.getRoutes();
+
+        for (int rout = 0; rout < routes.size(); rout++) {
+            String day = routes.get(rout).getDay();
+
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.MATCH_PARENT);
             Button btn = new Button(this);
-            btn.setId(i);
+            btn.setId(rout);
             final int id_ = btn.getId();
-            btn.setText("button " + id_);
-            params.setMargins(0,0,0,20);
+            btn.setText(day);
+            params.setMargins(0, 0, 0, 20);
             btn.setLayoutParams(params);
             btn.setHeight(60);
 
             linear.addView(btn, params);
             btn1 = ((Button) findViewById(id_));
             btn1.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    Toast.makeText(view.getContext(),
-                            "Button clicked index = " + id_, Toast.LENGTH_SHORT)
-                            .show();
-                }
-            });
+                    public void onClick(View view) {
+                        Toast.makeText(view.getContext(),
+                                "Button clicked index = " + id_, Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+            }
+
         }
 
+    public History getHistoryJSON() {
+        String filePath = getBaseContext().getFilesDir().getAbsolutePath() + "/" + fileName;
+        JSONResourceReader reader = new JSONResourceReader(filePath);
+        History jsonObj = reader.constructUsingGson(History.class);
+
+        return jsonObj;
     }
 }
